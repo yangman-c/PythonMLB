5e7890c7a3250671c6ba34d10f78a787bde51fe2
pengqihe <pengqihe@babeltime.com>
2020-08-31 11:19:23 +0800

    ranking

---
+++ ranking.bat
@@ -0,0 +1,9 @@
+@echo off
+if exist ranking\%today%.log (
+    del ranking\%today%.log
+)
+if not exist ranking (
+	md ranking
+)
+set today=%date:~0,4%-%date:~5,2%-%date:~8,2%
+python ranking.py > ranking\%today%.log
