e0ed573a4c6646b0b15e289c9bfcce0971294b27
pengqihe <pengqihe@babeltime.com>
2020-11-20 15:46:18 +0800

    daily update

--- klinePredict.py
+++ klinePredict.py
@@ -1,5 +1,12 @@
 import sys
 import pymysql
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.ensemble import GradientBoostingClassifier
+
+algom = (RandomForestClassifier(), DecisionTreeClassifier(), GaussianNB(), BernoulliNB())
 
 class KLinePredict:
     def connectDB(self)->pymysql.connect:
@@ -17,7 +24,7 @@ class KLinePredict:
         cursor = con.cursor()
         length = 0
         try:
-            length = cursor.execute('''select start,end,max,min,voc,tor from s_{} order by date desc limit 120;'''.format(code))
+            length = cursor.execute('''select start,end,max,min,voc,tor from s_{} order by date desc limit 300;'''.format(code))
             # print("length:{}".format(length))
         except Exception as err:
             print("select err:{}".format(err))
@@ -42,25 +49,51 @@ class KLinePredict:
             voc = data[4]
             tor = data[5]
             old = end - voc
-            startP = start / old - 1
-            endP = end / old - 1
-            maxP = max / old - 1
-            minP = min / old - 1
+            startP = (start / old - 1) * 100
+            endP = (end / old - 1) * 100
+            maxP = (max / old - 1) * 100
+            minP = (min / old - 1) * 100
             xTrain.append((startP, endP, maxP, minP, tor))
-            yTrain.append(endP)
+            y = 0
+            if endP < -7.5:
+               y = -4
+            elif endP < -5:
+               y = -3
+            elif endP < -2.5:
+               y = -2
+            elif endP < 0:
+               y = -1
+            elif endP < 2.5:
+               y = 1
+            elif endP < 5:
+               y = 2
+            elif endP < 7.5:
+               y = 3
+            else:
+               y = 4
+            yTrain.append(y)
+        xTrain.reverse()
+        yTrain.reverse()
         xTrain = xTrain[:-1]
         yTrain = yTrain[1:]
         splitIndex = int(lengh * 0.75)
         xTest = xTrain[splitIndex:]
-        yTrain = yTrain[splitIndex:]
+        yTest = yTrain[splitIndex:]
         xTrain = xTrain[:splitIndex]
         yTrain = yTrain[:splitIndex]
         return xTrain, yTrain, xTest, yTest
 
+    def predict(self, xTrain, yTrain, xTest, yTest):
+        rf = GradientBoostingClassifier()
+        rf.fit(xTrain, yTrain)
+        print(rf.score(xTest, yTest))
+
+
     def run(self, code:str):
         con = self.connectDB()
         allData = self.readDB(con, code)
-        self.makeDataset(allData)
+        xTrain, yTrain, xTest, yTest = self.makeDataset(allData)
+        self.predict(xTrain, yTrain, xTest, yTest)
 
 
 if __name__ == '__main__':
