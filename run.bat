49b48725f76f2bcdd3bbd26dbd4d3dea564e9166
pengqihe <pengqihe@babeltime.com>
2021-01-25 15:45:52 +0800

    daily update

--- run.bat
+++ run.bat
@@ -3,10 +3,10 @@ echo datasets.bat
 call datasets.bat
 echo ranking.bat
 call ranking.bat
-echo report.bat
-call report.bat
-echo record.bat
-call record.bat
+#echo report.bat
+#call report.bat
+#echo record.bat
+#call record.bat
 git add .
 git c -m "daily update"
 git push
